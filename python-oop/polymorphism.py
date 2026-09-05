257868aaa9df8b60b83b5555f04c21a381ffc017
Bishaleyyyyy <techbishal566@gmail.com>
2026-09-05 08:56:26 +0545

    polymorphism

--- python-oop/polymorphism.py
+++ python-oop/polymorphism.py
@@ -1,43 +1,82 @@
+# from abc import ABC, abstractmethod
+
+# class Animal(ABC):
+
+#     @abstractmethod
+#     def sound(self):
+#         pass
+
+#     @abstractmethod
+#     def move(self):
+#         pass
+
+
+# class Dog(Animal):
+#     def sound(self):
+#         print("The dog is barking!")
+
+#     def move(self):
+#         print("The dog is running!")
+
+
+# class Bird(Animal):
+#     def sound(self):
+#         print("The bird is chirping!")
+
+#     def move(self):
+#         print("The bird is flying in the air!")
+
+
+# class Fish(Animal):
+#     def sound(self):
+#         print("The fish is silent...")
+
+#     def move(self):
+#         print("The fish is swimming under the water!")
+
+
+# animals = [Dog(), Bird(), Fish()]
+
+# for animal in animals:
+#     animal.sound()
+#     animal.move()
+#     print("----------")
+
+
+
+# *****--Another example---------**
+
 from abc import ABC, abstractmethod
 
-class Animal(ABC):
+class Shape(ABC):
 
     @abstractmethod
-    def sound(self):
+    def area(self):
         pass
 
-    @abstractmethod
-    def move(self):
-        pass
+class Circle(Shape):
+    def __init__(self, radius):
+       self.radius = radius
 
+    def area(self):
+        return 3.14 * self.radius ** 2
 
-class Dog(Animal):
-    def sound(self):
-        print("The dog is barking!")
+class Square(Shape):
+    def __init__(self,side):
+      self.side = side
 
-    def move(self):
-        print("The dog is running!")
+    def area(self):
+        return self.side **2
 
+class Rectangle(Shape):
+    def __init__(self, length,breadth):
+        self.length = length
+        self.breadth = breadth
 
-class Bird(Animal):
-    def sound(self):
-        print("The bird is chirping!")
+    def area(self):
+        return self.length * self.breadth
 
-    def move(self):
-        print("The bird is flying in the air!")
+shapes = [Circle(4), Square(3), Rectangle(5,5)]
 
-
-class Fish(Animal):
-    def sound(self):
-        print("The fish is silent...")
-
-    def move(self):
-        print("The fish is swimming under the water!")
-
-
-animals = [Dog(), Bird(), Fish()]
-
-for animal in animals:
-    animal.sound()
-    animal.move()
-    print("----------")
+for shape in shapes:
+    print(shape.area())
